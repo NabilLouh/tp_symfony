fc3e64884906df3b7ccaef29d88af1277ee87480
Arkhin <nabillouh62110@gmail.com>
2022-09-01 14:49:05 +0200

    fin base de donnée et affichage

--- src/Controller/EvenementController.php
+++ src/Controller/EvenementController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Evenement;
+use App\Repository\EvenementRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +13,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class EvenementController extends AbstractController
 {
     #[Route('/evenement', name: 'app_evenement')]
-    public function index()
+    public function index(ManagerRegistry $doctrine, EvenementRepository $repository)
     {
-        return $this->render('evenement/index.html.twig');
+        $evenements = $repository->findAll(); 
+
+        dump($evenements);
+
+        return $this->render('evenement/index.html.twig', [
+            'evenements' => $evenements,
+        ]);
+    }
+
+    #[Route('/evenement/{id}', name: 'app_evenement_show')]
+    public function show(EvenementRepository $repository, Evenement $evenement)
+    {
+        return $this->render('evenement/show.html.twig', [
+            'evenement' => $evenement,
+        ]);
     }
 
     #[Route('/evenement/create', name: 'app_evenement_create')]
